09ac4459ebf3462d36684563176f30ae8d48c273
Daniel Trinh <dantrinh@seas.upenn.edu>
2018-11-19 10:27:48 -0800

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/show', to: 'pages#show'
+  get 'pages/about', to: 'pages#about'
+
+
+end
