475e2908a8c8ef127e0225855b43a737a2a17047
Daniel Trinh <dantrinh@seas.upenn.edu>
2018-11-19 14:02:08 -0800

    creating before action helper function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -20,15 +21,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(sign_up_params)
       flash[:notice] = "successfully edited"
       redirect_to article_path(@article)
@@ -38,13 +36,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "article has been deleted"
     redirect_to articles_path
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def sign_up_params
     params.require(:article).permit(:title, :description)
   end
