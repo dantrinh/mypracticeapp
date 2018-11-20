591b8f9337b26c6e05a505bee95044b1ffadaf6f
Daniel Trinh <dantrinh@seas.upenn.edu>
2018-11-19 17:36:08 -0800

    Add password to migrations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,6 @@ class User < ActiveRecord::Base
             uniqueness: {case_sensitive: false},
             format: { with: VALID_EMAIL_REGEX },
             length: {maximum: 105}
+  has_secure_password
 
 end
